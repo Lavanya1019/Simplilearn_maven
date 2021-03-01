0e27ccbb859d666b32c0deb4a716e36fde2d1fbe
Lavanya1019 <78846023+Lavanya1019@users.noreply.github.com>
2021-03-01 05:27:48 +0000

    Add files via upload

---
+++ src/test/java/pages/Loginpage.java
@@ -0,0 +1,60 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Loginpage {
+	WebDriver driver;
+	
+	@FindBy(linkText="Log in")
+	WebElement lnkLogin;
+	
+	
+	@FindBy(name="user_login")
+	WebElement editUsername;
+	
+	@FindBy(name="user_pwd")
+	WebElement editPwd;
+//	
+//	@FindBy(className="rememberMe")
+//	WebElement chkBox;
+	
+	@FindBy(name="btn_login")
+	WebElement btnPwd;
+	public Loginpage(WebDriver testdriver) 
+	{
+		this.driver=testdriver;
+		
+		PageFactory.initElements(driver,this);
+	}
+	
+	
+	public void Login(WebDriver testdriver) 
+	{
+		this.driver=testdriver;
+	}
+	
+	public void Login(String UserName,String password) {
+		
+		//WebElement lnkLogin=driver.findElement(By.linkText("Log in"));
+		
+		lnkLogin.click();
+		
+        //WebElement editUserName=driver.findElement(By.name("user_login"));
+        editUsername.sendKeys(UserName);
+        
+        //WebElement editPwd=driver.findElement(By.name("user_pwd"));
+        editPwd.sendKeys(password);
+        
+      // WebElement btnPwd=driver.findElement(By.name("btn_login"));
+        btnPwd.click();
+	}
+	
+	
+	
+	
+
+}
